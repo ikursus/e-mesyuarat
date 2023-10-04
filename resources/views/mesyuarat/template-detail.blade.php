002ecc3ddf54164189c0a5940f6be7be4209edcd
Amirol Zolkifli <sitehandy@gmail.com>
2023-10-04 12:04:00 +0800

    Update template detail mesyuarat

--- resources/views/mesyuarat/template-detail.blade.php
+++ resources/views/mesyuarat/template-detail.blade.php
@@ -78,23 +78,37 @@
                 </div>
 
                 <!-- Modal -->
-                <div class="modal fade" id="tambah-ahli" tabindex="-1" aria-labelledby="tambah-ahliLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="tambah-ahliLabel">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <form method="POST" action="">
+                    @csrf
+                    <div class="modal fade" id="tambah-ahli" tabindex="-1" aria-labelledby="tambah-ahliLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="tambah-ahliLabel">Modal title</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+
+                                <div class="form-group">
+                                    <label>Ahli</label>
+                                    <select class="form-control" name="user_id">
+                                        <option value="">-- Sila Pilih --</option>
+
+                                        @foreach ($senaraiUser as $ahli)
+                                        <option value="{{ $ahli->id }}">{{ $ahli->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                        ...
-                        </div>
-                        <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
-                    </div>
-                </div>
+                </form>
 
             </div>
         </div>
