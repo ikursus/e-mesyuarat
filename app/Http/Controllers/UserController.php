84c6169da08fd8f2cfaa1d3cd83552d7f0fb2613
Amirol Zolkifli <sitehandy@gmail.com>
2023-10-05 09:58:53 +0800

    Update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $senaraiUsers = User::get();
+        $senaraiUsers = User::all();
 
         return view('users.template-senarai', compact('senaraiUsers'));
     }
@@ -37,6 +37,10 @@ class UserController extends Controller
             'password' => ['required', 'min:3', 'confirmed']
         ]);
 
+        $data = $request->all();
+
+        //dd($data);
+
         // Cara 1 untuk simpan data ke table users menggunakan Model
         // $user = new User;
         // $user->name = $data['name']; // $request->input('name')
@@ -55,17 +59,18 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        return view('users.template-detail', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    //public function edit(string $id)
+    public function edit(User $user)
     {
-        $user = User::find($id);
+        // $user = User::find($id); // User::findOrFail($id);
 
         return view('users.template-edit', compact('user'));
     }
@@ -74,8 +79,34 @@ class UserController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
+    // public function update(Request $request, User $user)
     {
-        //
+        $data = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email:filter'],
+            'phone' => ['nullable', 'sometimes']
+        ]);
+
+        // Validation password JIKA password diisi untuk ditukar
+        if ($request->has('password') && $request->filled('password'))
+        {
+            $request->validate([
+                'password' => ['min:3', 'confirmed']
+            ]);
+
+            $data['password'] = $request->input('password');
+        }
+
+        // Update Data User
+        // Cara 1 menggunakan carian user
+        $user = User::find($id);
+        $user->update($data);
+
+        // Cara 2
+        // User::where('id', '=', $id)->update($data);
+
+        // Redirect ke halaman senarai mesyuarat selepas proses kemaskini data.
+        return redirect()->route('users.index')->with('mesej-sukses', 'Rekod berjaya dikemaskini');
     }
 
     /**
--- resources/views/users/template-detail.blade.php
+++ resources/views/users/template-detail.blade.php
@@ -3,16 +3,16 @@
 @section('main-content')
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Maklumat Mesyuarat</h1>
+        <h1 class="mt-4">Maklumat User</h1>
         <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Maklumat Mesyuarat</li>
+            <li class="breadcrumb-item active">Maklumat User</li>
         </ol>
 
 
         <div class="card mb-4">
             <div class="card-header">
                 <i class="fas fa-table me-1"></i>
-                Maklumat Mesyuarat - {{ $mesyuarat->perkara }}
+                Maklumat User - {{ $user->name }}
             </div>
             <div class="card-body">
 
@@ -30,28 +30,16 @@
 
                         <tbody>
                             <tr>
-                                <td>PERKARA</td>
-                                <td>{{ $mesyuarat->perkara }}</td>
+                                <td>NAMA</td>
+                                <td>{{ $user->name }}</td>
                             </tr>
                             <tr>
-                                <td>TARIKH</td>
-                                <td>{{ $mesyuarat->tarikh }}</td>
+                                <td>EMAIL</td>
+                                <td>{{ $user->email }}</td>
                             </tr>
                             <tr>
-                                <td>MASA MULA</td>
-                                <td>{{ $mesyuarat->masa_mula }}</td>
-                            </tr>
-                            <tr>
-                                <td>MASA TAMAT</td>
-                                <td>{{ $mesyuarat->masa_tamat }}</td>
-                            </tr>
-                            <tr>
-                                <td>LOKASI</td>
-                                <td>{{ $mesyuarat->lokasi }}</td>
-                            </tr>
-                            <tr>
-                                <td>STATUS</td>
-                                <td>{{ $mesyuarat->status }}</td>
+                                <td>PHONE</td>
+                                <td>{{ $user->phone }}</td>
                             </tr>
 
                         </tbody>
@@ -66,17 +54,17 @@
         <div class="card mt-4 mb-4">
             <div class="card-header">
                 <i class="fas fa-table me-1"></i>
-                Maklumat Ahli Terlibat
+                Maklumat Mesyuarat Terlibat
             </div>
             <div class="card-body">
 
                 <!-- Button trigger modal -->
                 <div class="mt-3">
-                    <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambah-ahli">Tambah Ahli</button>
+                    <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambah-ahli">Daftarkan Mesyuarat</button>
                 </div>
 
                 <!-- Modal -->
-                <form method="POST" action="{{ route('mesyuarat.ahli.store', $mesyuarat->id) }}">
+                <form method="POST" action="{{ route('mesyuarat.ahli.store', $user->id) }}">
                     @csrf
                     <div class="modal fade" id="tambah-ahli" tabindex="-1" aria-labelledby="tambah-ahliLabel" aria-hidden="true">
                         <div class="modal-dialog">
@@ -92,9 +80,9 @@
                                     <select class="form-control" name="user_id">
                                         <option value="">-- Sila Pilih --</option>
 
-                                        @foreach ($senaraiAhli as $ahli)
+                                        {{-- @foreach ($senaraiAhli as $ahli)
                                         <option value="{{ $ahli->id }}">{{ $ahli->name }}</option>
-                                        @endforeach
+                                        @endforeach --}}
                                     </select>
                                 </div>
 
@@ -113,52 +101,12 @@
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>NAMA</th>
+                            <th>MESYUARAT</th>
                             <th>TINDAKAN</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($senaraiAhliMesyuarat as $ahli)
-                        <tr>
-                            <td>{{ $ahli->id }}</td>
-                            <td>{{ $ahli->ahli_name }}</td>
-                            <td>
-                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-delete-{{ $ahli->id }}">Keluarkan</button>
 
-                                <!-- Modal Delete -->
-                                <form method="POST" action="{{ route('mesyuarat.ahli.destroy', $mesyuarat->id) }}">
-
-                                    @csrf
-                                    @method('DELETE')
-
-                                    <div class="modal fade" id="modal-delete-{{ $ahli->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Pengesahan</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-
-                                                Adakah anda bersetuju untuk menghapuskan data {{ $ahli->ahli_name }}
-                                                <input type="hidden" name="user_id" value="{{ $ahli->user_id }}">
-                                                <input type="hidden" name="mesyuarat_user_id" value="{{ $ahli->id }}">
-
-                                            </div>
-                                            <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-danger">Ya, Delete!</button>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </div>
-
-                                </form>
-
-
-                            </td>
-                        </tr>
-                        @endforeach
                     </tbody>
                 </table>
 
